e5b637f290602a0edd952e28022b7d32871e1469
walid.terrak <walid.terrak.pro@gmail.com>
2024-09-12 09:37:02 +0200

    v1.0.2

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-a = int(1)
-print(a)
+# a = "bonjour"
+# b = "bonsoir"
+
+# print(id(a))
+# print(id(b))
+
+# a = 5
+# c = a
+# c = 10
+# print(id(a))
+# print(id(c))
+
+# a = 15
+# b = 10
+
+# print(a*b)
+
+def hello_paul():
+    prenom = "paul"
+    print(prenom)
+    
